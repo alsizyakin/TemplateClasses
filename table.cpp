7978cbfffac5010fad2ae5adb6a282cc0e12e318
alsizyakin <al.sizyakin@gmail.com>
2021-11-01 22:32:13 +0300

    more interesting solve from authors

--- table.cpp
+++ table.cpp
@@ -7,20 +7,12 @@ using namespace std;
 template <typename T>
 class Table
 {
-    size_t strok;
-    size_t stolb;
     vector<vector<T>> data;
 
 public:
     Table(size_t a, size_t b)
     {
-        strok = a;
-        stolb = b;
-        data.resize(strok);
-        for (vector<T>& stl : data)
-        {
-            stl.resize(stolb);
-        }
+        Resize(a,b);
     };
 
     vector<T> &operator[](size_t index)
@@ -29,35 +21,31 @@ public:
     };
 
     const vector<T> &operator[](size_t index) const{
-        cout << "const op is called";
         return data[index];
     };
 
     void Resize(size_t a_new, size_t b_new)
     {
-        strok = a_new;
-        stolb = b_new;
-        data.resize(strok);
+        data.resize(a_new);
         for (auto& stl : data)
         {
-            stl.resize(stolb);
+            stl.resize(b_new);
         }
     };
 
     pair<size_t, size_t> Size() const
     {
-        if (strok == 0 or stolb == 0)
+        if (data.empty() or data[0].empty())
         {
             return {0, 0};
         }
-        return {strok, stolb};
+        return {data.size(), data[0].size()};
     };
 };
 
 void TestTable()
 {
     Table<int> t(1, 1);
-    cout << t.Size().first << " " << t.Size().second << endl;
     ASSERT_EQUAL(t.Size().first, 1u);
     ASSERT_EQUAL(t.Size().second, 1u);
     t[0][0] = 42;
@@ -69,7 +57,6 @@ void TestTable()
 
 int main()
 {
-    cout << "Starts ff" << endl;
     TestRunner tr;
     RUN_TEST(tr, TestTable);
     return 0;
