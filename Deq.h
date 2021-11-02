8ca14ac8bc9b87264de5cb28fe517f95b262d86d
alsizyakin <al.sizyakin@gmail.com>
2021-11-03 01:46:15 +0300

    deque solved

--- Deq.h
+++ Deq.h
@@ -1,52 +1,126 @@
-#if !defined DEQUE
+#ifndef DEQUE
 #define DEQUE
 
 #include <exception>
+#include <stdexcept>
 #include <vector>
+#include <iostream>
+using namespace std;
 
-template<teypename T>
-class Deque{
+template <typename T>
+class Deque
+{
 
 private:
-
-vector<T> front;
-vector<T> back;
+    vector<T> front;
+    vector<T> back;
 
 public:
-bool Empty() const{
+    Deque()
+    {
+        front = {};
+        back = {};
+    }
 
+    bool Empty() const
+    {
+        return (front.empty() && back.empty());
+    };
+
+    size_t Size() const
+    {
+        return (front.size() + back.size());
+    };
+
+    T &operator[](size_t index)
+    {
+        if (front.empty() || index > (front.size() - 1))
+        {
+            return back[index - front.size()];
+        }
+        return front[front.size() - 1 - index];
+    };
+
+    const T &operator[](size_t index) const
+    {
+        if (front.empty() || index > (front.size() - 1))
+        {
+            return back[index - front.size()];
+        }
+        return front[front.size() - 1 - index];
+    };
+
+    T &At(size_t index)
+    {
+        if (index > (back.size() + front.size() - 1))
+        {
+            throw out_of_range("index is too large");
+        }
+        if (front.empty() || index > (front.size() - 1))
+        {
+            return back[index - front.size()];
+        }
+        return front[front.size() - 1 - index];
+    };
+
+    const T &At(size_t index) const
+    {
+        if (index > (back.size() + front.size() - 1))
+        {
+            throw out_of_range("index is too large");
+        }
+        if (front.empty() || index > (front.size() - 1))
+        {
+            return back[index - front.size()];
+        }
+        return front[front.size() - 1 - index];
+    };
+
+    T &Front()
+    {
+        if (front.empty())
+        {
+            return back[0];
+        }
+        return front.back();
+    }
+
+    const T &Front() const
+    {
+        if (front.empty())
+        {
+            return back[0];
+        }
+        return front.back();
+    }
+
+    T &Back()
+    {
+        if (back.empty())
+        {
+            return front[0];
+        }
+        return back.back();
+    }
+
+    const T &Back() const
+    {
+        if (back.empty())
+        {
+            return front[0];
+        }
+        return back.back();
+    }
+
+    void PushFront(T front_el)
+    {
+        front.push_back(front_el);
+    };
+
+    void PushBack(T back_el)
+    {
+        back.push_back(back_el);
+    };
 };
-
-size_t Size() const{
-
-};
-
-T& operator[](size_t index) {
-
-};
-
-const T& operator[](size_t index) const{
-
-};
-
-T& At(size_t index){
-
-};
-
-
-const T& At(size_t index) const{
-
-};
-
-void PushFront(T font_el){
-
-};
-
-void PushBack(T back_el){
-
-};
-
-}
-
 
 #endif
--- deq.cpp
+++ deq.cpp
@@ -1,7 +1,49 @@
 #include "Deq.h"
+#include "test_runner.h"
 
-int main(){
+
+void TestDeq()
+{
+    Deque<int> d;
+    ASSERT_EQUAL(d.Size(), 0u);
+    ASSERT_EQUAL(d.Empty(), true);
+    d.PushBack(3);
+    //d.PushFront(3);
+    ASSERT_EQUAL(d.Empty(), false);
+    ASSERT_EQUAL(d.Size(), 1u);
+    ASSERT_EQUAL(d[0], 3);
+
+    d.PushFront(4);
+    ASSERT_EQUAL(d.Empty(), false);
+    ASSERT_EQUAL(d.Size(), 2u);
+    ASSERT_EQUAL(d[0], 4);
+    ASSERT_EQUAL(d[1], 3);
+    ASSERT_EQUAL(d.At(0), 4);
+    ASSERT_EQUAL(d.At(1), 3);
+
+    d.PushFront(7);
+    ASSERT_EQUAL(d.Empty(), false);
+    ASSERT_EQUAL(d.Size(), 3u);
+    ASSERT_EQUAL(d[0], 7);
+    ASSERT_EQUAL(d[1], 4);
+    ASSERT_EQUAL(d[2], 3);
+    ASSERT_EQUAL(d.At(0), 7);
+    ASSERT_EQUAL(d.At(1), 4);
+    ASSERT_EQUAL(d.Front(), 7);
+    ASSERT_EQUAL(d.Back(), 3);
+    //ASSERT_EQUAL(d.At(2), 3);
+    
+    //ASSERT_EQUAL(d.At(12), "index is too large");
 
 
+
+}
+
+int main()
+{
+    TestRunner tr;
+    cout << "Start of tests built at " << __TIME__ << endl;
+    RUN_TEST(tr, TestDeq);
+    cout << "End of tests built at " << __TIME__ << endl;
     return 0;
 }
